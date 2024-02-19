ad7951c69088aaa086d56c9abf728b11c6787fe5
Jordon <98439832+jordoncodes@users.noreply.github.com>
2024-02-19 21:13:11 +1000

    update nickapi + bump version

--- src/main/java/me/onlyjordon/capehider/CapeHider.java
+++ src/main/java/me/onlyjordon/capehider/CapeHider.java
@@ -4,6 +4,7 @@ import me.onlyjordon.capehider.commands.CommandManager;
 import me.onlyjordon.capehider.commands.SimpleCommandManager;
 import me.onlyjordon.nicknamingapi.Nicknamer;
 import me.onlyjordon.nicknamingapi.NicknamerAPI;
+import me.onlyjordon.nicknamingapi.events.PlayerSkinLayerChangeEvent;
 import me.onlyjordon.nicknamingapi.utils.SkinLayers;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -31,6 +32,17 @@ public final class CapeHider extends JavaPlugin implements Listener {
     public void onJoin(PlayerJoinEvent event) {
         if (!event.getPlayer().hasPermission("capehider.bypass"))
             nicknamer.setSkinLayerVisible(event.getPlayer(), SkinLayers.SkinLayer.CAPE, false);
+    }
+
+    @EventHandler
+    public void onSkinLayerChange(PlayerSkinLayerChangeEvent event) {
+        if (event.getReason() == PlayerSkinLayerChangeEvent.Reason.PLUGIN) return;
+        SkinLayers layers = event.getNewLayers();
+        SkinLayers oldLayers = event.getPreviousLayers();
+        if (layers == null) return;
+        boolean enabled = oldLayers != null && oldLayers.isLayerVisible(SkinLayers.SkinLayer.CAPE);
+        layers.setLayerVisible(SkinLayers.SkinLayer.CAPE, enabled);
+        event.setNewLayers(layers);
     }
 
     @Override
